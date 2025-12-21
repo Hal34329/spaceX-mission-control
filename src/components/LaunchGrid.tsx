8fe084752da2fbe3fa68090b5686e074c45e144e
Hal34329 <jesusguillen545@gmail.com>
2025-12-21 12:21:49 -0600

    Added new css file to organize theme and components

--- src/components/LaunchGrid.tsx
+++ src/components/LaunchGrid.tsx
@@ -15,9 +15,9 @@ const LaunchGrid = ({ launches }: LaunchGridProps) => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" className="icon icon-tabler icons-tabler-outline icon-tabler-search" viewBox="0 0 24 24"><path fill="none" stroke="none" d="M0 0h24v24H0z"/><path d="M3 10a7 7 0 1 0 14 0 7 7 0 1 0-14 0m18 11-6-6"/></svg>
                 <input type="text" placeholder="Search mission name, flight number..." name="searchText" className="searchText flex-1 ml-2" />
             </form>
-            <button className="py-2.5 px-5 rounded-2xl bg-gray-800 text-white font-medium border border-solid border-transparent shrink-0 cursor-pointer">All missions</button>
-            <button className="py-2.5 px-5 rounded-2xl bg-gray-800 text-white font-medium border border-solid border-transparent shrink-0 cursor-pointer">Starlink</button>
-            <button className="py-2.5 px-5 rounded-2xl bg-gray-800 text-white font-medium border border-solid border-transparent shrink-0 cursor-pointer">Falcon 9</button>
+            <button className="btn-filter">All missions</button>
+            <button className="btn-filter">Starlink</button>
+            <button className="btn-filter">Falcon 9</button>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {launches.map(launch => (
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,33 +1,47 @@
 const Sidebar = () => {
+    const toggleTheme = () => {
+        const root = document.documentElement;
+
+        if(root.classList.contains("dark")){
+            root.classList.remove("dark");
+            localStorage.setItem("theme", "light");
+        } else {
+            root.classList.add("dark");
+            localStorage.setItem("theme", "dark");
+        }
+    }
+
     return(
         <>
         <aside className="w-64 shrink-0 bg-neutral-900 border-r border-neutral-800 sticky top-0 h-screen flex flex-col">
             {/* Navigation / Filters */}
             <nav className="p-4 space-y-2 border-b border-neutral-800">
                 <h3>MAIN MENU</h3>
-                <button className="w-full text-left px-3 py-2 rounded-lg hover:bg-neutral-800">
+                <button className="btn-nav">
                 All missions
                 </button>
-                <button className="w-full text-left px-3 py-2 rounded-lg hover:bg-neutral-800">
+                <button className="btn-nav">
                 Upcoming
                 </button>
-                <button className="w-full text-left px-3 py-2 rounded-lg hover:bg-neutral-800">
+                <button className="btn-nav">
                 Past Launches
                 </button>
-                <button className="w-full text-left px-3 py-2 rounded-lg hover:bg-neutral-800">
+                <button className="btn-nav">
                 Rockets
                 </button>
             </nav>
 
             <nav className="p-4 space-y-2 border-b border-neutral-800">
                 <h3>FILTERS</h3>
-                <button className="w-full text-left px-3 py-2 rounded-lg hover:bg-neutral-800">
+                <button className="btn-nav">
                 Successful
                 </button>
-                <button className="w-full text-left px-3 py-2 rounded-lg hover:bg-neutral-800">
+                <button className="btn-nav">
                 Failed
                 </button>
             </nav>
+
+            <button className="btn-filter" onClick={toggleTheme}>Color</button>
 
             {/* Profile / Header */}
             <div className="p-4 border-t border-neutral-800 mt-auto">
