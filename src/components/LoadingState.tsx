9a4b7830bf4beba486bc11a74ef4864513911eed
Hal34329 <jesusguillen545@gmail.com>
2025-12-22 21:08:05 -0600

    Removed comments and changed color themes from hex to hsl

--- src/components/LoadingState.tsx
+++ src/components/LoadingState.tsx
@@ -16,15 +16,6 @@ const LoadingState = () => {
 
   return (
     <div className="flex flex-col items-center justify-center h-[calc(100vh-2rem)] gap-6">
-      {/* <div className="relative w-16 h-16">
-        <div className="absolute inset-0 rounded-full border-4 
-        border-red-600
-        dark:border-emerald-splash" />
-        <div className="absolute inset-0 rounded-full border-4 
-        border-blue-700
-        dark:border-emerald-green border-t-transparent animate-spin" />
-      </div> */}
-
       <div className="relative size-16">
         <div className="absolute inset-0 rounded-full border-4 border-red-600 dark:border-emerald-splash" />     
         <div className="absolute inset-0 rounded-full border-4 border-transparent border-t-blue-500 dark:border-t-emerald-green animate-spin" />
